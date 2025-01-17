aac6e3a87dc67d7b6652a503aad05c88928ac019
Vishnu <vishnunarasim20@gmail.com>
2025-01-17 19:52:45 +0530

    Implemented Audit

--- src/main/java/com/bank/accountservice/AccountServiceApplication.java
+++ src/main/java/com/bank/accountservice/AccountServiceApplication.java
@@ -2,8 +2,12 @@ package com.bank.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+// Enable the JPA Auditing and please leverage the bean with the name auditAwareImpl
+// to understand the current auditor.
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/bank/accountservice/entity/BaseEntity.java
+++ src/main/java/com/bank/accountservice/entity/BaseEntity.java
@@ -1,10 +1,17 @@
 package com.bank.accountservice.entity;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -13,9 +20,26 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    // This attribute tells JPA that the value of this column
+    // should not be modified during the execution of any UPDATE statements.
     private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(updatable = false)
     private String createdBy;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    // This attribute tells JPA that the column
+    // should not be included in the INSERT SQL statement.
     private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(insertable = false)
     private String updatedBy;
 }
